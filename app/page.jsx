86032d137ab5619e4863ab99756d1908757c379a
synsynlele <your-email@example.com>
2026-04-19 00:08:38 +0100

    PipuPath V2 upgrade

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
+import {createClient } from "@/lib/supabase";
 
 /* =========================
    PREMIUM ARCHETYPES
